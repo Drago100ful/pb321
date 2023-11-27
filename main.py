01a4dde304a7460ae3b08c43a7d44bf099e6dc02
Chris Kerpa <chris.kerpa@mennen.online>
2023-11-27 12:11:53 +0100

    Added dual y axis. Changed plot size

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from glob import glob
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def generate_monthly_csv():
@@ -13,22 +13,28 @@ def generate_monthly_csv():
         dfs.append(pd.read_csv(path).head(200).set_index('Page'))
         dfs[i]["Date"] = datetime.strptime(path[len([files[0]]) - 12::].rstrip(".csv"), "%Y_%m").strftime("%Y-%m-%d")
 
-    df = pd.concat(dfs)
-    df = df.sort_values("Date", ascending=False)
-    df.to_csv("./dataset/out/topviews_merged.csv")
+    dfGen = pd.concat(dfs)
+    dfGen = dfGen.sort_values("Date", ascending=False)
+    dfGen.to_csv("./dataset/out/topviews_merged.csv")
 
 
 if __name__ == '__main__':
     # generate_monthly_csv()
+    plt.rcParams[("figure.figsize")] = [10, 5]
+    query = "Bible"
 
-    dfMonthly = pd.read_csv("dataset/out/topviews_merged.csv")
-    dfMonthly = dfMonthly.head(3600)
-    dfMonthlyChatGPT = dfMonthly[dfMonthly.Page.isin(["ChatGPT"])]
-    dfMonthly2 = dfMonthly[dfMonthly.Page.isin(["YouTube"])]
-    dfMonthly3 = dfMonthly[dfMonthly.Page.isin(["Donald Trump"])]
-    plt.plot(dfMonthlyChatGPT["Date"], dfMonthlyChatGPT["Views"], label='ChatGPT')
-    plt.plot(dfMonthly2["Date"], dfMonthly2["Views"], label='YouTube')
-    plt.plot(dfMonthly3["Date"], dfMonthly3["Views"], label='Donald Trump')
-    plt.legend()
+    dfMonthly = pd.read_csv("dataset/out/topviews_merged.csv").set_index("Date").sort_values("Date", ascending=True)
+    df = dfMonthly[dfMonthly.Page.isin([query])]
+
+    df["Views"].plot(title=query, label="Views", legend=True)
+    plt.ylabel("Views in millions")
+
+    mask = df["Edits"] == "?"
+    df[~mask]["Edits"].astype(int).plot(secondary_y=True, label="Edits", legend=True)
+    plt.ylabel("Edits")
+
+    mask = df["Editors"] == "?"
+    df[~mask]["Editors"].astype(int).plot(secondary_y=True, label="Editors", legend=True)
+
+    plt.gcf().autofmt_xdate(rotation=45)
     plt.show()
-    print(dfMonthly)
