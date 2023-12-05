7552f4fc69f5eda5dd6a0358a01c0c347d6c3f59
Chris Kerpa <chris.kerpa@mennen.online>
2023-12-05 10:08:54 +0100

    Fixed ? Datasets

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def generate_monthly_csv():
     files = glob('./dataset/monthly/*.csv')
 
     for i, path in enumerate(files):
-        dfs.append(pd.read_csv(path).head(200).set_index('Page'))
+        dfs.append(pd.read_csv(path).set_index('Page'))
+        dfs[i].loc[dfs[i]["Edits"] == "?", "Edits"] = "None"
+        dfs[i].loc[dfs[i]["Editors"] == "?", "Editors"] = "None"
         dfs[i]["Date"] = datetime.strptime(path[len([files[0]]) - 12::].rstrip(".csv"), "%Y_%m").strftime("%Y-%m-%d")
 
     dfGen = pd.concat(dfs)
@@ -20,21 +22,25 @@ def generate_monthly_csv():
 
 if __name__ == '__main__':
     # generate_monthly_csv()
+
     plt.rcParams[("figure.figsize")] = [10, 5]
-    query = "Bible"
+    query = "Oppenheimer (film)"
 
     dfMonthly = pd.read_csv("dataset/out/topviews_merged.csv").set_index("Date").sort_values("Date", ascending=True)
     df = dfMonthly[dfMonthly.Page.isin([query])]
 
-    df["Views"].plot(title=query, label="Views", legend=True)
+    plt.figure()
+    fig, ax = plt.subplots()
+
+    df["Views"].plot(ax=ax, title=query, x="Date", label="Views", legend=True)
     plt.ylabel("Views in millions")
 
     mask = df["Edits"] == "?"
-    df[~mask]["Edits"].astype(int).plot(secondary_y=True, label="Edits", legend=True)
+    df[~mask]["Edits"].plot(x="Date", y="Edits", ax=ax, secondary_y=True, label="Edits", legend=True)
     plt.ylabel("Edits")
 
-    mask = df["Editors"] == "?"
-    df[~mask]["Editors"].astype(int).plot(secondary_y=True, label="Editors", legend=True)
+    mask2 = df["Editors"] == "?"
+    df[~mask2]["Editors"].plot(ax=ax, x="Date", secondary_y=True, label="Editors", legend=True)
 
     plt.gcf().autofmt_xdate(rotation=45)
     plt.show()
